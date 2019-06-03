c23fccfa87702f0c6f26918522434eb04325847b
thato <thatokekana77@gmail.com>
2019-06-03 12:10:32 +0200

    sales

---
+++ src/main/java/za/co/shoprite/sales/Sales.java
@@ -0,0 +1,24 @@
+package za.co.shoprite.sales;
+
+public class Sales{
+
+Sales(){}
+
+public double total;
+public double cash;
+public double change;
+public double vat;
+
+public void totalCalculator(){
+System.out.print("Total");
+}
+
+public void changeCalculator(){
+System.out.print("Change");
+}
+
+public void vatCalculator(){
+System.out.print("Vat");
+}
+
+}
