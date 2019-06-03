db39e985e9d59aabfe7cbf9235c16d85163a3068
thato <thatokekana77@gmail.com>
2019-06-03 11:53:45 +0200

    cashier

---
+++ src/main/java/za/co/shoprite/staff/Cashier.java
@@ -0,0 +1,20 @@
+package za.co.shoprite.staff;
+
+public class Cashier{
+
+public Cashier(){}
+
+public String name;
+public long idNumber;
+public long contactNumber;
+public String emailAddress;
+public String physicalAddress;
+public String postalAddress;
+public int password;
+
+public void login(){
+System.out.println("Login");
+}
+
+
+}
